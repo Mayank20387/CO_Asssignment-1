d78a8f77023151ed26181771ee12034a48f807f7
Mayank20387 <mayank@LAPTOP-1R2Q53N4.localdomain>
2021-08-27 03:03:10 +0530

    fifth commit

--- SimpleSimulator/m4.py
+++ SimpleSimulator/m4.py
@@ -1,4 +1,4 @@
-import m3
+import m3   
 #import matplotlib
 #matplotlib.use('Agg')
 from matplotlib import pyplot as plt
@@ -7,7 +7,7 @@ pc_list=[]
 memory=[]
 registers=[]                   
 #registers=['0000000000000000', '0000000000000000', '0000000000000101', '0000000000000010', '0000000000000000', '0000000000000000', '0000000000000000']
-while True:
+while True:    #appending the input in list l1
     try :
         line = input()
         if line!="":
@@ -46,7 +46,7 @@ def RF(registers,register):
         registers.append("0000000000000000")
 
     return registers
-
+#appending keys and values present in dictionary(register) in two seperate lists
 def reg_no(string,register):
     k=-1
     register_keys = list(register.keys())
@@ -58,7 +58,7 @@ def reg_no(string,register):
 
 
 
-
+#main function
 def Execute(opcode,string,register,registers):
     global halted
     global pc
@@ -69,66 +69,70 @@ def Execute(opcode,string,register,registers):
     tempflag=FLAGS
     reset()
     max_int=65535
+    #addition instruction
     if string[:5]==opcode["add"][0]:
         b=int(registers[reg_no(string[10:13],register)],2)
         c=int(registers[reg_no(string[13:16],register)],2)
         a=b+c
+        #overflow flag is set if computation overflows
         if a>max_int:
             a=a-max_int
             changeflag('v')
 
         registers[reg_no(string[7:10],register)]=format(a,'016b')
-
+    #subtraction instruction
     elif string[:5]==opcode["sub"][0]:
         b=int(registers[reg_no(string[10:13],register)],2)
         c=int(registers[reg_no(string[13:16],register)],2)
+        #overflow flag is set if below condition fulfills and 0 is assigned to 'a'
         if c>b:
             a=0
             changeflag('v')
         else:
             a=b-c
         registers[reg_no(string[7:10],register)]=format(a,'016b')
-
+    #multiply instruction
     elif string[:5]==opcode["mul"][0]:
         b=int(registers[reg_no(string[10:13],register)],2)
         c=int(registers[reg_no(string[13:16],register)],2)
         a=b*c
+        #overflow flag is set if computation overflows
         if a>max_int:
             a=a-max_int
             changeflag('v')
         registers[reg_no(string[7:10],register)]=format(a,'016b')
-
+    #xor instruction
     elif string[:5]==opcode["xor"][0]:
         b=int(registers[reg_no(string[10:13],register)],2)
         c=int(registers[reg_no(string[13:16],register)],2)
         a=b^c
         registers[reg_no(string[7:10],register)]=format(a,'016b')
-
+    #or instruction
     elif string[:5]==opcode["or"][0]:
         b=int(registers[reg_no(string[10:13],register)],2)
         c=int(registers[reg_no(string[13:16],register)],2)
         a=b|c
         registers[reg_no(string[7:10],register)]=format(a,'016b')
-
+    #and instruction
     elif string[:5]==opcode["and"][0]:
         b=int(registers[reg_no(string[10:13],register)],2)
         c=int(registers[reg_no(string[13:16],register)],2)
         a=b&c
         registers[reg_no(string[7:10],register)]=format(a,'016b')
-
+    #move immediate instruction
     elif string[:5]=="00010":
         registers[reg_no(string[5:8],register)]="00000000"+string[8:]
-    
+    #right shift instruction
     elif string[:5]==opcode["rs"][0]:
         a=int(registers[reg_no(string[5:8],register)],2)
         a=a>>int(string[8:],2)
         registers[reg_no(string[5:8],register)]=format(a,'016b')
-
+    #left shift instruction
     elif string[:5]==opcode["ls"][0]:
         a=int(registers[reg_no(string[5:8],register)],2)
         a=a<<int(string[8:],2)
         registers[reg_no(string[5:8],register)]=format(a,'016b')
-
+    #divide instruction
     elif string[:5]==opcode["div"][0]:
         a=int(registers[reg_no(string[10:13],register)],2)
         b=int(registers[reg_no(string[13:16],register)],2)
@@ -136,82 +140,83 @@ def Execute(opcode,string,register,registers):
         r=a%b
         registers[0]=format(q,'016b')
         registers[1]=format(r,'016b')
-
+    #move register instruction
     elif string[:5]==opcode["mov"][0]:
         if string[13:16]=="111":
             registers[reg_no(string[10:13],register)]=tempflag
         else:    
             a=int(registers[reg_no(string[13:16],register)],2)
             registers[reg_no(string[10:13],register)]=format(a,'016b')
-
+    #not instruction
     elif string[:5]==opcode["not"][0]:
         a=int(registers[reg_no(string[13:16],register)],2)
         b=~a
         registers[reg_no(string[10:13],register)]=format(b,'016b')
-
+    #compare instruction
     elif string[:5]==opcode["cmp"][0]:
         a=int(registers[reg_no(string[10:13],register)],2)
         b=int(registers[reg_no(string[13:16],register)],2)
         reset()
+        #sets greater than,less than,equal flag depending upon the input given
         if a>b:
             changeflag('g')
         elif a<b:
             changeflag('l')
         elif a==b:
             changeflag('e')
-
+    #store instruction
     elif string[:5]==opcode["st"][0]:
         a=int(registers[reg_no(string[5:8],register)],2)
         b=format(a,'016b')
         location=int(string[8:],2)
-        y.append(location)
-        x.append(cycle)
+        y.append(location) #y is a list containing memory address
+        x.append(cycle) #x is a list containing cycle no
         M1.addtomemory(b,location) 
-
+    #load instruction
     elif string[:5]==opcode["ld"][0]:
         location=int(string[8:],2)
         a=int(M1.getData(location),2)
         registers[reg_no(string[5:8],register)]=format(a,'016b')
-
+    #unconditional jump instruction
     elif string[:5]==opcode["jmp"][0]:
         location=int(string[8:],2)
         pc=location-1
 
-
+    #Jump If Greater Than instruction
     elif string[:5]==opcode["jgt"][0]:
         if tempflag=="0000000000000010":
             location=int(string[8:],2)
             pc=location-1
-
+    #Jump If less Than instruction
     elif string[:5]==opcode["jlt"][0]:
         if tempflag=="0000000000000100":
             location=int(string[8:],2)
             pc=location-1
-    
+    #Jump If equal instruction
     elif string[:5]==opcode["je"][0]:
         if tempflag=="0000000000000001":
             location=int(string[8:],2)
             pc=location-1
-
+    #hlt instruction
     elif string[:5]==opcode["hlt"][0]:
-        halted= True
+        halted= True #this will stop the code from executing
                
     return registers
 
-def reset():
+def reset():  #resetting the flags value
     global FLAGS
     FLAGS="0000000000000000"
 
 
-def changeflag(string):
+def changeflag(string):   #setting the value for flags register
     global FLAGS
-    if string=="v":
+    if string=="v":  #overflow
         FLAGS="0000000000001000"
-    elif string=="l":
+    elif string=="l": #less-than
         FLAGS="0000000000000100"
-    elif string=="g":
+    elif string=="g": #greater-than
         FLAGS="0000000000000010" 
-    elif string=="e":
+    elif string=="e":  #equal
         FLAGS="0000000000000001"
         
 
@@ -234,6 +239,7 @@ while not halted:
     cycle+=1
     #x.append(cycle)
 M1.dump()
+#for plotting graph
 plt.scatter(x,y)
 plt.savefig('plot.png', dpi=300, bbox_inches='tight')
 plt.show()
